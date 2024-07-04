6b9ebe0bfdb07d6f7b44cc70c85a6ac0fb416af6
Razmik Babayan <officialbabayan@gmail.com>
2024-07-04 17:09:31 +0400

    Add project files.

---
+++ Algorithms/BinarySearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class BinarySearch
+    {
+        public int? binary_search(int[] arr, int number)
+        {
+            int start = 0;
+            int end = arr.Length;
+            int mid = 0;
+            int midNumber = 0;
+
+            while (start <= end)
+            {
+                mid = (start + end) / 2;
+                midNumber = arr[mid];
+
+                if (midNumber == number)
+                {
+                    return mid;
+                }
+                else if (midNumber > number)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,15 @@
+﻿using Algorithms;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arr = new int[] { 1, 3, 5, 7, 9 };
+
+        BinarySearch binaySearch = new BinarySearch();
+        int? index = binaySearch.binary_search(arr, 9);
+
+        Console.Write("Binary Search : index = ");
+        Console.WriteLine(index != null ? index : "null");
+    }
+}
